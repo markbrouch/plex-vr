b19fba5742d375a090294fb8834e11618e12ccc7
Mark Brouch <mark.brouch@gmail.com>
2017-05-07 12:49:29 -0500

    📦 Install redux-persist and localForage

--- client/store.js
+++ client/store.js
@@ -1,7 +1,9 @@
 import { createStore, applyMiddleware, compose } from 'redux'
+import { persistStore, autoRehydrate } from 'redux-persist'
 import { createLogger } from 'redux-logger'
 import thunk from 'redux-thunk'
 import promiseMiddleware from 'redux-promise-middleware'
+import localForage from 'localForage'
 
 import rootReducer from '~reducers'
 
@@ -20,10 +22,15 @@ if (process.env.NODE_ENV !== 'production') {
   middleware.push(createLogger())
 }
 
-const enhancer = composeEnhancers(applyMiddleware(...middleware))
+const enhancer = composeEnhancers(
+  applyMiddleware(...middleware),
+  autoRehydrate()
+)
 
 export const configureStore = initialState => {
   const store = createStore(rootReducer, initialState, enhancer)
+
+  persistStore(store, { storage: localForage })
 
   if (module.hot) {
     module.hot.accept('~reducers', () => {
