4a88ee5359341198d6a4588a7624fe6d7f3ec5a9
Mark Brouch <mark.brouch@gmail.com>
2017-05-10 00:16:02 -0500

    🔧 Configure server logging

--- src/server/index.js
+++ src/server/index.js
@@ -1,11 +1,15 @@
 const Koa = require('koa')
+const koaBunyanLogger = require('koa-bunyan-logger')
 const Router = require('koa-router')
-const logger = require('koa-logger')
 const serve = require('koa-static')
 
 const req = require('request')
 const { resolve } = require('path')
 const { createReadStream } = require('fs')
+
+const logger = koaBunyanLogger.bunyan.createLogger({
+  name: 'server'
+})
 
 const PORT = process.env.PORT || 8000
 const STATIC_DIR = resolve(__dirname, '../dist')
@@ -14,11 +18,18 @@ const app = new Koa()
 const router = new Router()
 
 router.get('/transcode', ctx => {
-  const { 'plex-server': plexServer, ...headers } = ctx.request.headers
+  const { 'x-remote-server': plexServer, ...headers } = ctx.request.headers
 
   ctx.body = plexServer
 })
 
-app.use(logger()).use(router.routes()).use(serve(STATIC_DIR))
+app
+  .use(koaBunyanLogger({ name: 'server' }))
+  .use(koaBunyanLogger.requestIdContext())
+  .use(koaBunyanLogger.requestLogger())
+  .use(router.routes())
+  .use(serve(STATIC_DIR))
 
 app.listen(PORT)
+
+logger.info(`🌎 Listening on port ${PORT}`)
